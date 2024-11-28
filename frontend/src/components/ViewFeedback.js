4817e9c31a6f3c9fa15fa98e15e4e405d6056290
Shubham-Sharmaaa <154622767+Shubham-Sharmaaa@users.noreply.github.com>
2024-11-29 01:20:53 +0530

    Update ViewFeedback.js

--- frontend/src/components/ViewFeedback.js
+++ frontend/src/components/ViewFeedback.js
@@ -6,7 +6,7 @@ function ViewFeedback() {
 
   const fetchFeedback = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/feedback');
+      const response = await axios.get('https://tiet-feedback-api.vercel.app/feedback');
       setFeedbacks(response.data);
     } catch (error) {
       console.error('Error fetching feedback:', error);
